edf1024b63607a51afbf9ef9040e273426c8d514
teddyrendahl <trendahl@slac.stanford.edu>
2018-09-14 14:30:36 -0700

    DEP: Remove example scans

--- mfx/plans.py
+++ mfx/plans.py
@@ -1,9 +1,2 @@
-from bluesky.plan_stubs import open_run, mv, close_run
-from .beamline import tfs_trans
 
 
-def tfs_scan(start, stop):
-    yield from open_run()
-    yield from mv(tfs_trans, start)
-    yield from mv(tfs_trans, stop)
-    yield from close_run()
