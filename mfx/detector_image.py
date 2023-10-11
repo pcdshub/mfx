8a8badeedbaaca85a5800980a998df574f8fd4dd
themindatlarge <themindatlarge@gmail.com>
2023-10-11 00:30:47 -0700

    finished detector monitor

--- mfx/detector_image.py
+++ mfx/detector_image.py
@@ -1,6 +1,7 @@
 def detector_image(node=5, det='Rayonix', calibdir=None, ave=1):
     """
     Launches detector monitor
+
     Parameters
     ----------
     node: int, optional
@@ -23,15 +24,53 @@ def detector_image(node=5, det='Rayonix', calibdir=None, ave=1):
     import subprocess
     from mfx.macros import get_exp
 
+    experiment = str(get_exp())
+
     proc = [
-        f"ssh -YAC daq-mfx-mon0{node} "
-        f". /reg/g/psdm/sw/conda1/manage/bin/psconda.sh -py3; "
+        f"ssh -YAC mfxopr@daq-mfx-mon0{str(node)} "
         f"/cds/group/pcds/pyps/apps/hutch-python/mfx/scripts/"
-        f"detector_image.py -e {get_exp()} -d {det} -c {calibdir} -a {ave} &"
+        f"detector_image.sh {experiment} {str(det)} {str(calibdir)} {str(ave)}"
         ]
     
     logging.info(proc)
-
+	
     subprocess.Popen(
-        [proc], shell=True, 
+        proc, shell=True, 
         stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+
+def detector_image_kill(node=5):
+    """
+    Kills all detector monitors made with this script
+
+    Parameters
+    ----------
+    node: int, optional
+        Node to run detector monitor 1-9 only
+
+    det: str, optional
+        Detector name. Example 'Rayonix'
+
+    calibdir: str, optional
+        path to calib directory
+
+    ave: int, optional
+        Average over this number of events
+
+    Operations
+    ----------
+
+    """
+    import logging
+    import subprocess
+
+    proc = [
+        f"ssh -YAC mfxopr@daq-mfx-mon0{str(node)} " +
+        f"/cds/group/pcds/pyps/apps/hutch-python/mfx/scripts/"
+        f"detector_image_kill.sh"
+        ]
+    
+    logging.info(proc)
+	
+    subprocess.Popen(
+        proc, shell=True)
