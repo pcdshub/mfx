77e3a56cba6ed2ed542b2b5bc575c2d5651ef2ab
themindatlarge <themindatlarge@gmail.com>
2023-10-28 16:24:20 -0700

    fixed bug in detector_image.py

--- scripts/detector_image.sh
+++ scripts/detector_image.sh
@@ -6,6 +6,5 @@ calibdir=$3
 ave=$4
 
 source /reg/g/psdm/etc/psconda.sh
+conda activate ana-4.0.51-py3
 python /cds/group/pcds/pyps/apps/hutch-python/mfx/scripts/detector_image.py -e $experiment -d $det -c $calibdir -a $ave &
-
-#python /cds/group/pcds/pyps/apps/hutch-python/mfx/scripts/detector_image.py -e mfxl1013621
