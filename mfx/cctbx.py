f54a58c46181e2ef5e1056848af2dc132d60a5b5
themindatlarge <themindatlarge@gmail.com>
2024-06-12 00:54:36 -0700

    check_settings fixed

--- mfx/cctbx.py
+++ mfx/cctbx.py
@@ -8,67 +8,51 @@ class cctbx:
     def check_settings(self):
         import logging
         import subprocess
-       
+        exp = self.experiment
         logging.info("Checking xfel gui phil File")
         cctbx_settings = open("/cds/home/opr/mfxopr/.cctbx.xfel/settings_s3df.phil", "r", encoding="UTF-8")
         setting_lines = cctbx_settings.readlines()
         change = False
-        if setting_lines[4] != f'experiment =  "{self.experiment}"':
-            logging.warning(f"Changing experiment to current: {self.experiment}")
-            # setting_lines[4] = f'    experiment =  "{self.experiment}"\n'
-        settings = '''\
-        facility {
-          name = *lcls standalone
-          lcls {
-            experiment = "{exp}"
-            web {
-              location = "S3DF"
-            }
-          }
-        }
-        output_folder = "/sdf/data/lcls/ds/mfx/{exp}/results/common/results"
-        mp {
-          method = local lsf sge pbs *slurm shifter htcondor custom
-          nnodes_index = 2
-          nnodes_scale = 1
-          nnodes_merge = 1
-          nproc_per_node = 120
-          queue = "milano"
-          extra_options = " --account=lcls:{exp}  --reservation=lcls:onshift"
-          env_script = "/sdf/group/lcls/ds/tools/cctbx/build/conda_setpaths.sh"
-          phenix_script = "/sdf/group/lcls/ds/tools/cctbx/phenix/phenix-1.20.1-4487/phenix_env.sh"
-        }
-        experiment_tag = "common"
-        db {
-          host = "172.24.5.182"
-          name = "{exp}"
-          user = "{exp}"
-        }'''.format(exp=self.experiment)
-        change = True
-
-        # if setting_lines[10] != f'output_folder =  "/sdf/data/lcls/ds/mfx/{self.experiment}/results/common/result"':
-        #     logging.warning(f"Changing experiment to current: {self.experiment}")
-        #     setting_lines[10] = f'output_folder =  "/sdf/data/lcls/ds/mfx/{self.experiment}/results/common/result"\n'
-        #     change = True
-
-        # if setting_lines[10] != f'name = "{self.experiment}"':
-        #     logging.warning(f"Changing experiment to current: {self.experiment}")
-        #     setting_lines[10] = f'  name = "{self.experiment}"\n'
-        #     change = True
-
-        # if setting_lines[11] != f'user = "{self.experiment}"':
-        #     logging.warning(f"Changing experiment to current: {self.experiment}")
-        #     setting_lines[11] = f'  user = "{self.experiment}"\n'
-        #     change = True
-
+        if setting_lines[4] != f'experiment =  "{exp}"':
+            logging.warning(f"Changing experiment to current: {exp}")
+            change = True
+            settings = f'''\
+facility {{
+  name = *lcls standalone
+  lcls {{
+    experiment = "{exp}"
+    web {{
+      location = "S3DF"
+    }}
+  }}
+}}
+output_folder = "/sdf/data/lcls/ds/mfx/{exp}/results/common/results"
+mp {{
+  method = local lsf sge pbs *slurm shifter htcondor custom
+  nnodes_index = 2
+  nnodes_scale = 1
+  nnodes_merge = 1
+  nproc_per_node = 120
+  queue = "milano"
+  extra_options = " --account=lcls:{exp}  --reservation=lcls:onshift"
+  env_script = "/sdf/group/lcls/ds/tools/cctbx/build/conda_setpaths.sh"
+  phenix_script = "/sdf/group/lcls/ds/tools/cctbx/phenix/phenix-1.20.1-4487/phenix_env.sh"
+}}
+experiment_tag = "common"
+db {{
+  host = "172.24.5.182"
+  name = "{exp}"
+  user = "{exp}"
+}}\
+'''
         if change:
             cctbx_settings = open("/cds/home/opr/mfxopr/.cctbx.xfel/settings_new.phil", "w", encoding="UTF-8")
             cctbx_settings.writelines(settings)
             cctbx_settings.close
 
-        # subprocess.Popen(
-        #     ["rsync -avze ssh ~/.cctbx.xfel/settings_s3df.phil djr@s3dflogin:~/.cctbx.xfel/settings_old.phil "],
-        #     shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+            subprocess.Popen(
+                ["rsync -avze ssh ~/.cctbx.xfel/settings_s3df.phil djr@s3dflogin:~/.cctbx.xfel/settings_old.phil "],
+                shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 
     def xfel_gui(self):
