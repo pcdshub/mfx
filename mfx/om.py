a3781f1113db5cd61e9cf8e64eecc7d5b19d504f
themindatlarge <themindatlarge@gmail.com>
2024-12-10 17:39:54 -0800

    add check_settings to om

--- mfx/om.py
+++ mfx/om.py
@@ -2,6 +2,169 @@ class OM:
     def __init__(self):
         from mfx.macros import get_exp
         self.experiment = str(get_exp())
+        self.cwd = f'/cds/home/opr/mfxopr/OM-GUI'
+        self.pwd = f'{self.cwd}/{self.experiment}'
+
+
+    def fix_run_om(self, path: str):
+        """
+        Fixes the run_om.sh file to have current experiment number
+
+        Parameters:
+            path (str): run_om.sh file path.
+
+        Operations:
+
+        """
+        from subprocess import check_output
+        import logging
+        logging.info(f"Updating run_om.sh file: {path}")
+        wherepsana = check_output("wherepsana",shell=True).decode().strip('\n')
+
+        with open(path, "r") as file:
+            lines = file.readlines()
+            for ind, line in enumerate(lines):
+                if '--host' in lines[ind]:
+                    newline = f'     --host {wherepsana} $(pwd)/monitor_wrapper.sh\n'
+                    logging.info(f'Changing line {ind} to {newline}')
+                    lines[ind] = newline
+
+        with open(path, 'w') as file:
+            file.writelines(lines)
+
+
+    def fix_yaml(self, yaml: str, mask='', geom=''):
+        """
+        Fixes the yaml file to have current experiment number
+
+        Parameters:
+            yaml (str): monitor.yaml file path.
+
+            mask (str): mask file path.
+
+            geom (str): geom file path.
+
+        Operations:
+
+        """
+        import logging
+        logging.info(f"Updating yaml file: {yaml}")
+        with open(yaml, "r") as file:
+            lines = file.readlines()
+            for ind, line in enumerate(lines):
+                if 'psana_calibration_directory' in lines[ind]:
+                    newline = f'  psana_calibration_directory: /sdf/data/lcls/ds/mfx/{self.experiment}/calib\n'
+                    logging.info(f'Changing line {ind} to {newline}')
+                    lines[ind] = newline
+
+        if mask != '':
+            with open(yaml, "r") as file:
+                lines = file.readlines()
+                for ind, line in enumerate(lines):
+                    if 'bad_pixel_map_filename' in lines[ind]:
+                        newline = f'  bad_pixel_map_filename: {mask}\n'
+                        logging.info(f'Changing line {ind} to {newline}')
+                        lines[ind] = newline
+
+        if geom != '':
+            with open(yaml, "r") as file:
+                lines = file.readlines()
+                for ind, line in enumerate(lines):
+                    if 'geometry_file' in lines[ind]:
+                        newline = f'  geometry_file: {geom}\n'
+                        logging.info(f'Changing line {ind} to {newline}')
+                        lines[ind] = newline
+
+        with open(yaml, 'w') as file:
+            file.writelines(lines)
+
+
+    def check_settings(self):
+        """
+        Checks OM's file system and sets it up if needed
+
+        Parameters
+        ----------
+
+        Operations
+        ----------
+        """
+        import os
+        import sys
+        import logging
+        from shutil import copy2
+        logging.info("Checking OM Files")
+        if not os.path.exists(self.pwd) or len(os.listdir(self.pwd)) == 0:
+            logging.warning(f"No Directory Exists for Experiment: {self.experiment}. Would you like to create it?")
+            mkdir = input("(y/n)? ")
+
+            if mkdir.lower() == "y":
+                logging.info(f"Creating Directory for Experiment: {self.experiment}.")
+                pre_pwd = max(
+                    [os.path.join(self.cwd, d) for d in os.listdir(
+                        self.cwd) if os.path.isdir(os.path.join(self.cwd, d))], key=os.path.getmtime)
+
+                os.makedirs(self.pwd)
+                det_dirs=[f'{self.pwd}/om_workspace', f'{self.pwd}/om_workspace_rayonix', f'{self.pwd}/om_workspace_xes']
+                logging.info(f"Creating Directories for Epix10k2M.")
+                os.makedirs(det_dirs[0])
+                logging.info(f"Creating Directories for Rayonix.")
+                os.makedirs(det_dirs[1])
+                logging.info(f"Creating Directories for XES.")
+                os.makedirs(det_dirs[2])
+
+                pre_det_dirs=[f'{pre_pwd}/om_workspace', f'{pre_pwd}/om_workspace_rayonix', f'{pre_pwd}/om_workspace_xes']
+
+                logging.info(f"Copying Key Files from Previous Experiment: {pre_pwd}")
+
+                for ind, det in enumerate(det_dirs):
+                    logging.info(f"Copying Key Files for: {pre_det_dirs[ind]}")
+                    shell_list = [os.path.join(pre_det_dirs[ind], file) for file in os.listdir(
+                        pre_det_dirs[ind]) if file.endswith(".sh")]
+                    if len(shell_list) != 0:
+                        for sh in shell_list:
+                            copy2(sh, det_dirs[ind])
+                    else:
+                        logging.error(f'No shell scripts found in: {det}')
+
+                    if os.path.isfile(os.path.join(det, 'run_om.sh')):
+                        self.fix_run_om(os.path.join(det, 'run_om.sh'))
+
+                    geom_list = [os.path.join(
+                        pre_det_dirs[ind], file) for file in os.listdir(
+                            pre_det_dirs[ind]) if file.endswith(".geom")]
+                    if len(geom_list) != 0:
+                        geom = max(geom_list, key=os.path.getmtime)
+                        copy2(geom, det_dirs[ind])
+                    else:
+                        logging.error(f'No geom found in: {det}')
+
+                    mask_list = [os.path.join(
+                        pre_det_dirs[ind], file) for file in os.listdir(
+                            pre_det_dirs[ind]) if 'mask' in file]
+                    if len(mask_list) != 0:
+                        mask = max(mask_list, key=os.path.getmtime)
+                        copy2(mask, det_dirs[ind])
+                    else:
+                        logging.error(f'No mask found in: {det}')
+
+                    yaml_list = [os.path.join(
+                        pre_det_dirs[ind], file) for file in os.listdir(
+                            pre_det_dirs[ind]) if file.endswith(".yaml")]
+                    if len(yaml_list) != 0:
+                        yaml = max(yaml_list, key=os.path.getmtime)
+                        copy2(yaml, det_dirs[ind])
+                        self.fix_yaml(
+                            os.path.join(
+                                det_dirs[ind], os.path.basename(yaml)), mask=os.path.basename(mask), geom=os.path.basename(geom))
+                    else:
+                        logging.error(f'No yaml found in: {det}')
+
+                logging.info(f"Copying om_reset_plots.py")
+                copy2(os.path.join(pre_pwd, 'om_reset_plots.py'), self.pwd)
+            else:
+                logging.info(f"You've decided not to continue. Program will exit")
+                sys.exit()
 
 
     def gui(
@@ -74,8 +237,7 @@ class OM:
         """
         import logging
         import subprocess
-
-        pwd = f'/cds/home/opr/mfxopr/OM-GUI/{self.experiment}'
+        self.check_settings()
         monitor_wrapper = None
         monitor_wrapper = subprocess.check_output(
             "ssh -YAC mfx-monitor ps aux | grep monitor_wrapper.sh | grep -v grep | awk '{ print $2 }'",shell=True).decode()
@@ -83,11 +245,11 @@ class OM:
         det_dir = None
 
         if str(det).lower() == 'rayonix':
-                det_dir = f'{pwd}/om_workspace_rayonix'
+                det_dir = f'{self.pwd}/om_workspace_rayonix'
         elif str(det).lower() == 'epix10k2m':
-                det_dir = f'{pwd}/om_workspace'
+                det_dir = f'{self.pwd}/om_workspace'
         elif str(det).lower() == 'xes':
-                det_dir = f'{pwd}/om_workspace_xes'
+                det_dir = f'{self.pwd}/om_workspace_xes'
         else:
             logging.error(
                 "No proper detector seclected. Please use either"
@@ -126,9 +288,8 @@ class OM:
         import logging
         import subprocess
 
-        pwd = f'/cds/home/opr/mfxopr/OM-GUI/{self.experiment}'
         proc = [
-            f'source /cds/sw/ds/ana/conda1/manage/bin/psconda.sh | python {pwd}/om_reset_plots.py daq-mfx-mon10',
+            f'source /cds/sw/ds/ana/conda1/manage/bin/psconda.sh | python {self.pwd}/om_reset_plots.py daq-mfx-mon10',
             ]
 
         logging.info(proc)
