f56b5b8ce6b4097fb47a89cbdeed3b7ac954d9f8
djr_sdf <themindatlarge@gmail.com>
2024-12-12 23:25:24 -0800

    add refinement0

--- scripts/cctbx/image_viewer.sh
+++ scripts/cctbx/image_viewer.sh
@@ -19,27 +19,28 @@ case $facility in
     ;;
 esac
 
-if [[ ! -d ${mfx_dir}/common/results/averages/${run} ]]; then
-    echo ERROR: Run not averaged yet. Please stop and Average from the GUI
-    exit 1 # terminate and indicate error
-fi
+runpath="${mfx_dir}/common/results/averages/${run}"
 
-if [ -z "${group}" ]; then
-  # No arguments provided, open the newest file
-  group=$(ls -t ${mfx_dir}/common/results/averages/${run} | head -n 1)
-  echo "no trial-rungroup provided so using the newest"
-else
-  # Arguments provided, use the first one as the file to open
-  echo "path provided"
-fi
-
-out="${mfx_dir}/common/results/averages/${run}/${group}/out"
+out="${runpath}/${group}/out"
 cd ${out}
 dials.import max.cbf wavelength=1.105
 
 case $type in
 
   mask)
+    if [[ ! -d ${runpath} ]]; then
+        echo ERROR: Run not averaged yet. Please stop and Average from the GUI
+        exit 1 # terminate and indicate error
+    fi
+
+    if [ -z "${group}" ]; then
+      # No arguments provided, open the newest file
+      group=$(ls -t ${runpath} | head -n 1)
+      echo "no trial-rungroup provided so using the newest"
+    else
+      # Arguments provided, use the first one as the file to open
+      echo "path provided"
+    fi
     cd ${mfx_dir}/common/results
     mkdir masks
     cd masks/
@@ -49,6 +50,34 @@ case $type in
     ;;
 
   geometry)
+    if [[ ! -d ${runpath} ]]; then
+        echo ERROR: Run not averaged yet. Please stop and Average from the GUI
+        exit 1 # terminate and indicate error
+    fi
+
+    if [ -z "${group}" ]; then
+      # No arguments provided, open the newest file
+      group=$(ls -t ${runpath} | head -n 1)
+      echo "no trial-rungroup provided so using the newest"
+    else
+      # Arguments provided, use the first one as the file to open
+      echo "path provided"
+    fi
+    dials.image_viewer imported.expt
+    ;;
+
+  refinement0)
+    mkdir -p ${mfx_dir}/common/geom/refine_${group:3}
+    cd ${mfx_dir}/common/geom/refine_${group:3}
+    dials.combine_experiments ${mfx_dir}/common/results/r0*/${group}/out/*refined*.expt ${mfx_dir}/common/results/r0*/${group}/out/*indexed*.refl reference_from_experiment.detector=0
+    cctbx.xfel.detector_residuals combined.* hierarchy=1 tag=combined &
+    ;;
+
+  refinement1)
+    dials.image_viewer imported.expt
+    ;;
+
+  refinement)
     dials.image_viewer imported.expt
     ;;
 esac
