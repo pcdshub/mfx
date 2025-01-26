591ce6d6e8f44c5c112580205c816273ca880f06
fredericpoitevin <frederic.poitevin@gmail.com>
2025-01-25 16:36:57 -0800

    Wrote convenience tool for beam alignment. Not tested.

--- mfx/beamline.py
+++ mfx/beamline.py
@@ -82,6 +82,10 @@ with safe_load('OM'):
     from mfx.om import *
     om = OM()
 
+with safe_load('align'):
+    from mfx.optimize.align import *
+    align = Align()
+
 with safe_load('yano-kern_code'):
     from mfx.yano import *
     yano = yano()
---
+++ mfx/optimize/align.py
@@ -0,0 +1,48 @@
+class Align:
+    def __init__(self):
+        self.beam_alignment_diagnostics = ["DG1", "DG2"]
+        self.beam_alignment_methods = ["Xopt", "blop"]
+
+    def beam(
+            self,
+            on_diagnostic: str = "DG1",
+            with_method: str = 'Xopt'):
+        """Perform Beam Alignment
+
+        Parameters:
+
+            on_diagnostic (str): diagnostic to use for alignment. Options: "DG1, DG2".
+
+            with_method(str): method to use for alignment. Options: "blop, Xopt".
+        """
+        if on_diagnostic not in self.beam_alignment_diagnostics:
+            print(f"Diagnostic {diagnostic} not usable for beam alignment. Use {self.beam_alignment_diagnostics}.")
+            return None
+
+        if with_method not in self.beam_alignment_methods:
+            print(f"Alignment method {method} not implemented yet. Use {self.beam_alignment_methods}.")
+            return None
+
+        if method == 'Xopt':
+            from mfx.optimize.xopt_scans import get_xopt_obj, init_devices
+            xopt = get_xopt_obj(lower(diagnostic))
+            xopt.random_evaluate(3)
+            for num in range(10):
+                print(f"Step {num + 1}")
+                xopt.step()
+            _, val, params = xopt.vocs.select_best(xopt.data)
+            print(f"Best objective value {val}")
+            print(f"Best point {params}")
+            mirror_pitch = init_devices()["mr1l4_homs"].pitch
+            mirror_pitch.set(params["mirror_pitch"]).wait(timeout=20)
+            print(f"pitch is at {mirror_pitch.position}")
+            xopt.data.plot(y=xopt.vocs.objective_names)
+            return xopt
+        elif method == 'blop':
+            from mfx.optimize.blop_scans import get_blop_agent
+            agent = get_blop_agent(lower(diagnostic))
+            RE(agent.learn("qr", n=16))
+            RE(agent.learn("qei", n=4, iterations=4))
+            RE(agent.go_to_best())
+            agent.plot_objectives()
+            return agent
