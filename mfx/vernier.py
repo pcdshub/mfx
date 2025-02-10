992989595781fdab5100325515a397846e9ec7d1
themindatlarge <themindatlarge@gmail.com>
2025-02-10 02:13:38 -0800

    add energy series to venier

--- mfx/vernier.py
+++ mfx/vernier.py
@@ -41,13 +41,12 @@ class Vernier:
         for run in run_list:
             exp_run_list.append(f"{exp}:{run}")
 
-        runs = " ".join(exp_run_list)
         facility = facility.upper()
 
         proc = [
             f"ssh -Yt {user}@s3dflogin "
             f"python /sdf/group/lcls/ds/tools/mfx/scripts/cctbx/fee_spec.py "
-            f"-e {exp} -f {facility} -r {runs}"
+            f"-e {exp} -f {facility} -r {exp_run_list}"
             ]
 
         logging.info(proc)
@@ -142,4 +141,83 @@ class Vernier:
         pp.close()
         daq.disconnect()
         post(sample, tag, run_number, record, inspire)
+        logger.warning('Finished with all runs thank you for choosing the MFX beamline!\n')
+
+
+    def series(
+            self,
+            energy_scan_start_eV: float,
+            energy_scan_end_eV: float,
+            energy_scan_steps: int,
+            run_length: int = 10,
+            tag: str = None,
+            picker: str = None,
+            inspire: bool = False,
+            daq_delay: int = 5,
+            record: bool = False,
+            mcc_pv: str = 'MFX:USER:MCC:EPHOT:SET1'):
+        """Perform Vernier scan.
+
+        Parameters:
+            energy_scan_start_eV (float): 
+                Photon energy (in eV) to start the scan at.
+
+            energy_scan_end_eV (float): 
+                Photon energy (in eV) to end the scan at.
+
+            energy_scan_steps (int): 
+                Step Size (in eV).
+
+            run_length: int, optional
+                number of seconds for run 300 is default
+
+            tag: str, optional
+                Run group tag/sample name
+
+            picker: str, optional
+                If 'open' it opens pp before run starts. If 'flip' it flipflops before run starts
+
+            inspire: bool, optional
+                Set false by default because it makes Sandra sad. Set True to inspire
+
+            daq_delay: int, optional
+                delay time between runs. Default is 5 second but increase is the DAQ is being slow.
+
+            record (bool): 
+                whether to record the scan or not. Optional. Default: False.
+
+            mcc_pv (str): 
+                Vernier PV. Optional. Default: 'MFX:USER:MCC:EPHOT:SET1'.
+        
+
+        """
+        import os
+        import logging
+        from mfx.db import pp, daq
+        from mfx.autorun import quote, autorun
+        from time import sleep
+        logger = logging.getLogger(__name__)
+
+        if picker=='open':
+            pp.open()
+        if picker=='flip':
+            pp.flipflop()
+
+        logger.info(f"Run Number {daq.run_number() + 1} Running {sample}......{quote()['quote']}")
+        run_number = daq.run_number() + 1
+
+        energies = list(range(energy_scan_start_eV, energy_scan_end_eV, energy_scan_steps))
+
+        for ev in energies:
+            os.system(f'caput MFX:USER:MCC:EPHOT:SET1 {ev}')
+            
+            autorun(
+                sample=str(ev), 
+                tag=tag, 
+                run_length=run_length, 
+                record=record, 
+                inspire=inspire, 
+                picker=picker)
+            sleep(daq_delay)
+
         logger.warning('Finished with all runs thank you for choosing the MFX beamline!\n')
