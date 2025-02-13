12a9f6e65c4807d6d37a3745f7787d877477e6eb
Zachary Lentz <zlentz@slac.stanford.edu>
2025-02-12 18:10:50 -0800

    ENH: enable xopt scans with yag, misc formatting, include and modify in-situ align.py file

---
+++ mfx/optimize/align.py
@@ -0,0 +1,66 @@
+from bluesky import RunEngine
+
+
+class Align:
+    def __init__(self):
+        self.beam_alignment_diagnostics = ["DG1", "DG2"]
+        self.beam_alignment_methods = ["Xopt", "blop"]
+        self.beam_alignment_devices = ["yag", "wave8"]
+
+    def beam(
+            self,
+            with_goal: float,
+            on_diagnostic: str = "DG1",
+            with_method: str = 'Xopt',
+            using_device: str = "yag"):
+        """Perform Beam Alignment
+
+        Parameters:
+
+            on_diagnostic (str): diagnostic to use for alignment. Options: "DG1, DG2".
+
+            with_method(str): method to use for alignment. Options: "blop, Xopt".
+        """
+        if on_diagnostic not in self.beam_alignment_diagnostics:
+            print(f"Diagnostic {on_diagnostic} not usable for beam alignment. Use {self.beam_alignment_diagnostics}.")
+            return None
+
+        if with_method not in self.beam_alignment_methods:
+            print(f"Alignment method {with_method} not implemented yet. Use {self.beam_alignment_methods}.")
+            return None
+
+        if using_device not in self.beam_alignment_devices:
+            print(f"Only implemented devices: {self.beam_alignment_devices}")
+            return None
+
+        if with_method == 'Xopt':
+            from .xopt_scans import get_xopt_obj, init_devices
+            xopt = get_xopt_obj(
+                device_type=using_device,
+                location=on_diagnostic,
+                goal=with_goal,
+            )
+            xopt.random_evaluate(3)
+            for num in range(10):
+                print(f"Step {num + 1}")
+                xopt.step()
+            _, val, params = xopt.vocs.select_best(xopt.data)
+            print(f"Best objective value {val}")
+            print(f"Best point {params}")
+            mirror_pitch = init_devices()["mr1l4_homs"].pitch
+            mirror_pitch.set(params["mirror_pitch"]).wait(timeout=20)
+            print(f"pitch is at {mirror_pitch.position}")
+            xopt.data.plot(y=xopt.vocs.objective_names)
+            return xopt
+        elif with_method == 'blop':
+            from .blop_scans import get_blop_agent
+            try:
+                from mfx.db import RE
+            except ImportError:
+                RE = RunEngine({})
+            agent = get_blop_agent(on_diagnostic.lower(), wave8_xpos=with_goal)
+            RE(agent.learn("qr", n=16))
+            RE(agent.learn("qei", n=16, iterations=5))
+            RE(agent.go_to_best())
+            agent.plot_objectives()
+            return agent
--- mfx/optimize/mirror_hw.py
+++ mfx/optimize/mirror_hw.py
@@ -4,8 +4,12 @@ Initialize hardware for blop_scans and xopt_scans
 import random
 from types import SimpleNamespace
 
+import numpy as np
 from happi import Client
+from ophyd.areadetector.plugins import ImagePlugin
+from ophyd.device import Component as Cpt
 from ophyd.device import Device
+from ophyd.signal import AttributeSignal
 from ophyd.sim import SynAxis, SynSignal
 from pcdsdevices.ipm import Wave8
 
@@ -15,11 +19,21 @@ HAPPI_NAMES = (
     "mfx_dg2_ipm",
 )
 # Default constants so I can re-use them
+# Default starting point for searches
 MIRROR_NOMINAL = -544
+# Used for sim devices and as default goal positions
 DG1_WAVE8_XPOS = 8
 DG2_WAVE8_XPOS = 41
+DG1_YAG_XPOS = 320
+DG2_YAG_XPOS = 320
+IP_YAG_XPOS = 344
+
 
 devices: dict[str, Device] = {}
+
+
+class LCLSImagePlugin(ImagePlugin):
+    shaped_image = Cpt(AttributeSignal, "image", add_prefix=(), kind="omitted")
 
 
 def init_devices(force: bool = False) -> dict[str, Device]:
@@ -42,7 +56,16 @@ def init_devices(force: bool = False) -> dict[str, Device]:
         name = f"mfx_{stand}_wave8"
         devices[name] = Wave8(f"MFX:{stand.upper()}:BMMON", name=name)
         devices[name].kind = "hinted"
-    
+
+    # Happi PIMs don't work how I want them to, add manually
+    for stand in ("dg1", "dg2"):
+        name = f"mfx_{stand}_yag"
+        devices[name] = LCLSImagePlugin(f"MFX:{stand.upper()}:P6740:IMAGE1:", name=name)
+        devices[name].kind = "hinted"
+
+    devices["mfx_ip_yag"] = LCLSImagePlugin("MFX:GIGE:LBL:01:IMAGE1:", name="mfx_ip1_yag")
+    devices["mfx_ip_yag"].kind = "hinted"
+
     return devices
 
 
@@ -56,27 +79,51 @@ def sim_devices() -> dict[str, Device]:
     if devices:
         if isinstance(devices["mr1l4_homs"], SimpleNamespace):
             return devices
-    
+
     devices["mr1l4_homs"] = SimpleNamespace(pitch=SynAxis(name="mr1l4_homs_pitch", value=MIRROR_NOMINAL))
     devices["mfx_dg1_ipm"] = SimpleNamespace(inserted=True)
     devices["mfx_dg2_ipm"] = SimpleNamespace(inserted=False)
-    dg1_offset = random.uniform(-1, 1)
-    dg2_offset = random.uniform(-1, 1)
+    dg1_wave8_offset = random.uniform(-1, 1)
+    dg2_wave8_offset = random.uniform(-1, 1)
+    dg1_yag_offset = random.uniform(-30, 30)
+    dg2_yag_offset = random.uniform(-50, 50)
+    ip_yag_offset = random.uniform(-70, 70)
 
-    print("Generated fake dg1 and dg2 signals")
-    print("Expected: 1:1 linear relationship between x and pitch")
-    print(f"Expected: dg1 reads {DG1_WAVE8_XPOS + dg1_offset} + noise when pitch is {MIRROR_NOMINAL}")
-    print(f"Expected: dg2 reads {DG2_WAVE8_XPOS + dg2_offset} + noise when pitch is {MIRROR_NOMINAL}")
-    print(f"default alignment on dg1 should pick {MIRROR_NOMINAL - dg1_offset}")
-    print(f"default alignment on dg2 should pick {MIRROR_NOMINAL - dg2_offset}")
+    def get_fake_dg1_wave8() -> float:
+        pitch = devices["mr1l4_homs"].pitch.position
+        return pitch - MIRROR_NOMINAL + DG1_WAVE8_XPOS + dg1_wave8_offset + random.uniform(-0.1, 0.1)
 
-    def get_fake_dg1_wave8():
+    def get_fake_dg2_wave8() -> float:
         pitch = devices["mr1l4_homs"].pitch.position
-        return pitch - MIRROR_NOMINAL + DG1_WAVE8_XPOS + dg1_offset + random.uniform(-0.1, 0.1)
-    
-    def get_fake_dg2_wave8():
+        return pitch - MIRROR_NOMINAL + DG2_WAVE8_XPOS + dg2_wave8_offset + random.uniform(-0.1, 0.1)
+
+    def get_fake_dg1_yag() -> np.ndarray:
         pitch = devices["mr1l4_homs"].pitch.position
-        return pitch - MIRROR_NOMINAL + DG2_WAVE8_XPOS + dg2_offset + random.uniform(-0.1, 0.1)
+        return fake_yag_image(
+            size=(640, 480),
+            centroid=((pitch - MIRROR_NOMINAL) * 30 + DG1_YAG_XPOS + dg1_yag_offset + random.uniform(-3, 3), 240 + random.uniform(-3, 3)),
+            fwhm=30,
+            peak=255,
+        )
+
+    def get_fake_dg2_yag() -> np.ndarray:
+        pitch = devices["mr1l4_homs"].pitch.position
+        return fake_yag_image(
+            size=(640, 480),
+            centroid=((pitch - MIRROR_NOMINAL) * 50 + DG2_YAG_XPOS + dg2_yag_offset + random.uniform(-5, 5), 240 + random.uniform(-5, 5)),
+            fwhm=50,
+            peak=255,
+        )
+
+    def get_fake_ip_yag() -> np.ndarray:
+        pitch = devices["mr1l4_homs"].pitch.position
+        return fake_yag_image(
+            size=(688, 538),
+            centroid=((pitch - MIRROR_NOMINAL) * 70 + IP_YAG_XPOS + ip_yag_offset + random.uniform(-7, 7), 269 + random.uniform(-7, 7)),
+            fwhm=70,
+            peak=255,
+        )
+
 
     devices["mfx_dg1_wave8"] = SimpleNamespace(
         xpos=SynSignal(
@@ -90,7 +137,56 @@ def sim_devices() -> dict[str, Device]:
             name="mfx_dg2_wave8_xpos"
         )
     )
+    devices["mfx_dg1_yag"] = SimpleNamespace(
+        shaped_image=SynSignal(
+            func=get_fake_dg1_yag,
+            name="mfx_dg1_yag_shaped_image",
+        )
+    )
+    devices["mfx_dg2_yag"] = SimpleNamespace(
+        shaped_image=SynSignal(
+            func=get_fake_dg2_yag,
+            name="mfx_dg2_yag_shaped_image",
+        )
+    )
+    devices["mfx_ip_yag"] = SimpleNamespace(
+        shaped_image=SynSignal(
+            func=get_fake_ip_yag,
+            name="mfx_ip_yag_shaped_image",
+        )
+    )
     devices["mfx_dg1_wave8"].kind = "hinted"
     devices["mfx_dg2_wave8"].kind = "hinted"
-    
+    devices["mfx_dg1_yag"].kind = "hinted"
+    devices["mfx_dg2_yag"].kind = "hinted"
+    devices["mfx_ip_yag"].kind = "hinted"
+
+    print("Generated fake dg1 and dg2 signals and images")
+    print("Expected: 1:1 linear relationship between x and pitch")
+    print(f"Expected: dg1 wave8 reads {DG1_WAVE8_XPOS + dg1_wave8_offset} + noise when pitch is {MIRROR_NOMINAL}")
+    print(f"Expected: dg2 wave8 reads {DG2_WAVE8_XPOS + dg2_wave8_offset} + noise when pitch is {MIRROR_NOMINAL}")
+    print(f"Expected: dg1 yag reads {DG1_YAG_XPOS + dg1_yag_offset} + noise when pitch is {MIRROR_NOMINAL}")
+    print(f"Expected: dg2 yag reads {DG2_YAG_XPOS + dg2_yag_offset} + noise when pitch is {MIRROR_NOMINAL}")
+    print(f"Expected: dg2 yag reads {IP_YAG_XPOS + ip_yag_offset} + noise when pitch is {MIRROR_NOMINAL}")
+    print(f"default alignment on dg1 wave8 should pick {MIRROR_NOMINAL - dg1_wave8_offset}")
+    print(f"default alignment on dg2 wave8 should pick {MIRROR_NOMINAL - dg2_wave8_offset}")
+    print(f"default alignment on dg1 yag should pick {MIRROR_NOMINAL - dg1_yag_offset}")
+    print(f"default alignment on dg2 yag should pick {MIRROR_NOMINAL - dg2_yag_offset}")
+    print(f"detault alignment on ip yag should pick {MIRROR_NOMINAL - ip_yag_offset}")
+
     return devices
+
+
+def fake_yag_image(
+    size: tuple[int, int],
+    centroid: tuple[int, int],
+    fwhm: int,
+    peak: int,
+) -> np.ndarray:
+    # Adapted from https://stackoverflow.com/questions/7687679/how-to-generate-2d-gaussian-with-python
+    x = np.arange(0, size[0], 1, float)
+    y = np.arange(0, size[1], 1, float)[:, np.newaxis]
+    x0 = centroid[0]
+    y0 = centroid[1]
+    arr = np.exp(-4*np.log(2) * ((x-x0)**2 + (y-y0)**2) / fwhm**2)
+    return arr * peak
