d42dec98637f74783574abae264b0fe5a1cca636
Zachary Lentz <zlentz@slac.stanford.edu>
2025-04-23 17:01:39 -0700

    DOC: add some docstrings for the centroid path plotting.

--- mfx/optimize/plots.py
+++ mfx/optimize/plots.py
@@ -17,6 +17,27 @@ def centroid_path_plot(
     centroids: list[tuple[float, float]],
     figure: Optional[matplotlib.figure.Figure] = None,
 ) -> matplotlib.figure.Figure:
+    """
+    Plot the path of a centroid alignment.
+
+    The will include:
+    - The YAG image in the background
+    - The goal as a red x
+    - The measured centroids as white dots
+    - The camviewer markers as green + marks
+
+    This is intended to help verify that the alignment
+    is doing something useful and to debug when it does
+    something unexpected. For example, if our coordinate
+    system has an issue or the markers are set strangely
+    it will become more obvious using this plot.
+
+    Parameters
+    ----------
+    image : np.ndarray
+        The actual image of the YAG as a background.
+    
+    """
     fig = plt.figure(figure)
     plt.clf()
     plt.imshow(image, "cividis")
@@ -31,6 +52,19 @@ def centroid_path_plot(
 
 
 class UpdatingDeviceCentroidPathPlot:
+    """
+    Helper class for maintaining state and getting data for the path plot.
+
+    This is intended to help us create a useful plot that updates
+    as the scan proceeds.
+
+    Parameters
+    ----------
+    imager : YagCamera
+        The camera device instance.
+    goal : tuple[float, float]
+        The position we'd like the centroid to reach.
+    """
     def __init__(self, imager: YagCamera, goal: tuple[float, float]):
         self.fig = None
         self.imager = imager
@@ -38,6 +72,7 @@ class UpdatingDeviceCentroidPathPlot:
         self.points = []
 
     def get_markers(self) -> list[tuple[int, int]]:
+        """Helper to get the relevant global marker positions from the imager."""
         markers = []
         for num in range(4):
             try:
@@ -48,10 +83,21 @@ class UpdatingDeviceCentroidPathPlot:
         return markers
 
     def add_point(self, centroid: tuple[float, float]):
+        """
+        Add a new centroid to the plot and re-render.
+
+        Parameters
+        ----------
+        centroid : tuple[float, float]
+            The point to add.
+        """
         self.points.append(centroid)
         self.refresh()
 
     def refresh(self):
+        """
+        Re-render without adding any points, e.g. to update the YAG image.
+        """
         self.fig = centroid_path_plot(
             image=self.imager.image1.image,
             goal=self.goal,
