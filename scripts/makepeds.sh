8d7963f02d0c5c4e2e244182c538c2bf0e6539b1
themindatlarge <themindatlarge@gmail.com>
2025-04-26 23:53:03 -0700

    add daq2 to makepeds

--- scripts/makepeds.sh
+++ scripts/makepeds.sh
@@ -1,6 +1,30 @@
 #! /bin/bash
 
-run=$1
-experiment=$2
+daq=$1
+det=$2
+exp=$3
+run=$4
 
-ssh -Y psana /sdf/group/lcls/ds/tools/engineering_tools/engineering_tools/scripts/makepeds_psana --queue milano --run $run --experiment $experiment
+case $daq in
+  1)
+    ssh -Y psana /sdf/group/lcls/ds/tools/engineering_tools/engineering_tools/scripts/makepeds_psana --queue milano --run $run --experiment $experiment
+    ;;
+
+  2)
+    source /sdf/group/lcls/ds/ana/sw/conda2/manage/bin/psconda.sh
+
+    case $det in
+
+    jungfrau)
+        echo making jungfrau
+        jungfrau_dark_proc -k exp=$exp,run=$run -d jungfrau -o ~/work
+        jungfrau_deploy_constants -k exp=$exp,run=$run -d jungfrau -o ~/work -D
+        ;;
+
+    epix)
+        echo making epix
+        det_dark_proc -k "{'exp':'$exp','run':$run,'dir':'/sdf/data/lcls/drpsrcf/ffb/mfx/$exp/xtc/','detectors':['epix100']}" -d epix100 -D
+        ;;
+    esac
+    ;;
+esac
