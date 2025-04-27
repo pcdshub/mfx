268cd766ee996cd06ac457363bc25eb355051a08
fredericpoitevin <frederic.poitevin@gmail.com>
2025-04-26 21:20:08 -0700

    automfx commissioning - live hotfixes with beam

--- mfx/optimize/plots.py
+++ mfx/optimize/plots.py
@@ -164,12 +164,12 @@ class UpdatingXoptVisualizeModelPlot:
         """
         Re-render the new plot in place.
         """
-        if self.axes is not None:
-            for ax in self.axes:
-                ax.clear()
+        #if self.axes is not None:
+        #    for ax in self.axes:
+        #        ax.clear()
         self.fig, self.axes = self.xopt.generator.visualize_model(
             show_acquisition=False,
-            axes=self.axes
+            #axes=self.axes
         )
         plt.figure(self.fig)
         refresh_mpl_plots()
