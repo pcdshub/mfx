6786326e900daf09f5c6a60d3c66fd1a9666c124
themindatlarge <themindatlarge@gmail.com>
2025-04-28 02:24:01 -0700

    make mfx4

---
+++ mfx4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+exp=$1
+daq=$2
+
+HERE="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
+
+# shellcheck disable=SC1091
+source "${HERE}/mfxenv"
+
+if [ -n "$exp" ] || [ $daq == 1 ]; then
+    # Launch alt hutch-python with all devices and functions
+    python conf_edit.py -e $exp -d $daq
+    hutch-python --cfg "${HERE}/alt_conf.yml" "$@"
+else
+    # Launch hutch-python with all devices and functions
+    hutch-python --cfg "${HERE}/conf.yml" "$@"
+fi
