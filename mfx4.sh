39529a7d85ecf20a9420ad985f45f2b5b66c3298
themindatlarge <themindatlarge@gmail.com>
2025-04-28 02:25:54 -0700

    fix path

--- mfx4.sh
+++ mfx4.sh
@@ -9,7 +9,7 @@ source "${HERE}/mfxenv"
 
 if [ -n "$exp" ] || [ $daq == 1 ]; then
     # Launch alt hutch-python with all devices and functions
-    python conf_edit.py -e $exp -d $daq
+    python /reg/g/pcds/pyps/apps/hutch-python/mfx/scripts/conf_edit.py -e $exp -d $daq
     hutch-python --cfg "${HERE}/alt_conf.yml" "$@"
 else
     # Launch hutch-python with all devices and functions
