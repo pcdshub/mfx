405924f3e4cf2495db61668c9b0dd9914c92cb97
themindatlarge <themindatlarge@gmail.com>
2025-04-28 04:05:56 -0700

    temp solve for post in autorun for daq1

--- mfx4.sh
+++ mfx4.sh
@@ -8,9 +8,15 @@ HERE="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 # shellcheck disable=SC1091
 source "${HERE}/mfxenv"
 
-if [ -n "$exp" ] || [ $daq == 1 ]; then
+if [ -n "$exp" ]; then
     # Launch alt hutch-python with all devices and functions
     su $user -c "python /reg/g/pcds/pyps/apps/hutch-python/mfx/scripts/conf_edit.py -e $exp"
+else
+    echo "no yaml update requested"
+fi
+
+if [ $daq == 1 ]; then
+    # Launch alt hutch-python with all devices and functions
     hutch-python --cfg "${HERE}/alt_conf.yml" ${@:4}
 else
     # Launch hutch-python with all devices and functions
