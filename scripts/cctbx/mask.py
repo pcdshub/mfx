38b617768f0b640ab78b341581995f6ecb70fa67
fredericpoitevin <frederic.poitevin@gmail.com>
2025-05-24 19:07:09 -0700

    troubleshooting cctbx masking at S3df

--- scripts/cctbx/mask.py
+++ scripts/cctbx/mask.py
@@ -23,8 +23,8 @@ def mask(exp, run, facility, group):
     elif facility == "S3DF":
         exp=f'{exp}'  #fix this
         mfx_dir=f"/sdf/data/lcls/ds/mfx/{exp}/results"
-        out_path=f"{mfx_dir}/averages/{run}/{group}/out"
-        mask_path=f"{mfx_dir}/masks"
+        out_path=f"{mfx_dir}/common/averages/{run}/{group}/out"
+        mask_path=f"{mfx_dir}/common/masks"
     else:
         logging.warning(f"Facility not found: {facility}")
 
