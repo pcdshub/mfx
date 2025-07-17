dbc8cd6ce3c1ea2285ed7b1c7b6faec021ebf8f1
fredericpoitevin <frederic.poitevin@gmail.com>
2025-07-17 00:11:48 -0700

    Introducing geomrun().

--- mfx/autorun.py
+++ mfx/autorun.py
@@ -187,7 +187,7 @@ def ioc_cam_recorder(cam='camera name', run_length=10, tag='?'):
             shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
 
 
-def autorun(sample='?', tag=None, run_length=300, record=True,
+def _autorun(sample='?', tag=None, run_length=300, record=True,
             runs=5, inspire=False, daq_delay=5, picker=None, cam=None, close=True, daq_num=2):
     """
     Automate runs.... With optional quotes
@@ -244,8 +244,11 @@ def autorun(sample='?', tag=None, run_length=300, record=True,
     if picker=='flip':
         pp.flipflop()
 
+    run_type="SAMPLE"
     if tag is None:
         tag = sample
+    else:
+        run_type = tag
 
     if daq_num == 1:
         for i in range(runs):
@@ -326,8 +329,7 @@ def autorun(sample='?', tag=None, run_length=300, record=True,
                     daq.control.setRecord(True)
                 else:
                     daq.control.setRecord(False)
-
-                daq.control.setState("running")
+                daq.control.setState("running", {"run_type": run_type})
                 while daq.control.getState() != "running":
                     ...
                 start_time = time()
@@ -393,6 +395,12 @@ def autorun(sample='?', tag=None, run_length=300, record=True,
     else:
         logger.error('Please enter daq 1 or 2.')
 
+def autorun(**kwargs):
+    _autorun(**kwargs)
+
+def geomrun(**kwargs):
+    kwargs['tag'] = "GEOM"
+    _autorun(**kwargs)
 
 post_template = """\
 Run Number {}: {}
