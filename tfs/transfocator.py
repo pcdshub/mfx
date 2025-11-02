62ad06b31d1ae09e4ae31a9fcdf495c40514ac46
themindatlarge <themindatlarge@gmail.com>
2025-11-02 09:56:06 -0800

    prefocus fallback

--- tfs/transfocator.py
+++ tfs/transfocator.py
@@ -572,7 +572,7 @@ class MFXTransfocator(TransfocatorBase):
 
     def track_focus(self, energies, *, margin_mm=10.0, show=False,
                     ref_focal_length_um=None, ref_z_stage_mm=None, 
-                    display=True, shrinking_rate=4, **kwargs):
+                    display=True, shrinking_rate=4, enable_prefocus=True, **kwargs):
         """
         Keep the focal length fixed over a provided list of energies by
         compensating with the translation stage. Lenses are NOT actuated.
@@ -617,12 +617,15 @@ class MFXTransfocator(TransfocatorBase):
                     energy, combo, target_z_stage_mm, track_record
                 )
             else:
+                found_combo = False
                 prefocus_fallback = True
                 while prefocus_fallback:
                     shrinking_max_z_stage_mm = max_z_stage_mm
                     while shrinking_max_z_stage_mm > min_z_stage_mm:
                         self.mv_stage_to_pos(shrinking_max_z_stage_mm)
-                        combo = self.find_best_combo(energy_eV=energy, show=show, **kwargs)
+                        combo = self.find_best_combo(
+                            energy_eV=energy, show=show, enable_prefocus=enable_prefocus, **kwargs
+                        )
                         if combo:
                             new_target_z_stage_mm = self.get_z_stage_target(
                                 energy, combo, ref_focal_length_um, ref_z_stage_mm
@@ -631,9 +634,10 @@ class MFXTransfocator(TransfocatorBase):
                                 self.mv_stage_to_target_pos(
                                     energy, combo, new_target_z_stage_mm, track_record
                                 )
+                                found_combo = True
                                 break
                         shrinking_max_z_stage_mm -= shrinking_rate*margin_mm
-                    if combo:
+                    if found_combo:
                         break
                     else:
                         print("Stage out of travel. Cannot compensate further...")
