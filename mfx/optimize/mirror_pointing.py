b79c5cb7598c97ebb6a51695fe956cb61b2e6ae8
amine-lamouchi <amine.lamouchi@polytechnique.edu>
2026-03-04 23:46:52 -0800

    bugfix

--- mfx/optimize/mirror_pointing.py
+++ mfx/optimize/mirror_pointing.py
@@ -49,9 +49,15 @@ def optimize_mirror_pointing(instrument="mfx", diagnostic="MFX:GIGE:DG1:YAG:", w
     yag.image1.kind = "omitted"
     yag.num_frames = num_frames
 
+    # we don't have put permissions to enable callbacks, clearing the signals avoids this error
+    if instrument == "mec":
+        yag.image1.configuration_attrs = []
+        yag.image1.stage_sigs.clear()
+
     pv = "MR1L4:PITCH:MFX:Coating1" if instrument == "mfx" else "MR1L4:PITCH:MEC:Coating1"
     nominal = caget(pv)
 
+    # mec's markers are not global and can't be read from mfx, we're using marker positions from a recent ecperiment
     if instrument == "mec":
         goal = mec_goal
     else:
