56b2c67c686fc6b1ef79620b574e5d79cec8a5ef
sdehe <dehe@slac.stanford.edu>
2026-06-22 16:58:28 -0700

    fixed the dot wire motors

--- mfx/beamline.py
+++ mfx/beamline.py
@@ -166,7 +166,7 @@ with safe_load('Get_Info'):
 
 with safe_load('Wire_Scan'):
     from mfx.wire import *
-    wire = Wire(x_pv = 'MFX:USR:MMN:41', y_pv = 'MFX:USR:MMN:42')
+    wire = Wire(x_pv = 'MFX:USR:MMN:42', y_pv = 'MFX:USR:MMN:41')
 
 with safe_load('EXAFS'):
     from mfx.exafs import *
